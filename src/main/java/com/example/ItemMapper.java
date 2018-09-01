441dabb1760ed82d97b6137d0c7464dea2888199
Daniel Son <danielsonjh@hotmail.com>
2018-09-01 12:38:03 -0700

    Added imports

--- src/main/java/com/example/ItemMapper.java
+++ src/main/java/com/example/ItemMapper.java
@@ -1,10 +1,12 @@
 package com.example;
 
+import com.example.domain.ImmutableItem;
 import com.example.domain.Item;
+import com.example.dto.ImmutableItemDTO;
 import com.example.dto.ItemDTO;
 import org.mapstruct.Mapper;
 
-@Mapper
+@Mapper(imports = {ImmutableItem.class, ImmutableItemDTO.class})
 public abstract class ItemMapper {
     public abstract Item a(ItemDTO itemDTO);
     public abstract ItemDTO a(Item item);
